57b392b740697df319ed94d725822c7a64f10cb6
nkmtdak <tanemaki@hotmail.com>
2024-09-12 14:19:25 +0900

    clean

--- db/migrate/20240911095822_add_similarity_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddSimilarityToPhotos < ActiveRecord::Migration[7.0]
-  def change
-    add_column :photos, :similarity, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,56 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_11_095822) do
-  create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", charset: "utf8mb4", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "challenges", charset: "utf8mb4", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2024_09_10_090255) do
+  create_table "challenges", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
     t.bigint "user_id", null: false
+    t.boolean "cleared", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "cleared", default: false
     t.index ["user_id"], name: "index_challenges_on_user_id"
   end
 
-  create_table "photos", charset: "utf8mb4", force: :cascade do |t|
+  create_table "photos", charset: "utf8", force: :cascade do |t|
     t.bigint "challenge_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "similarity"
     t.index ["challenge_id"], name: "index_photos_on_challenge_id"
     t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", default: "", null: false
     t.string "password_digest"
@@ -67,16 +38,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_11_095822) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "encrypted_password", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "challenges", "users"
   add_foreign_key "photos", "challenges"
   add_foreign_key "photos", "users"
