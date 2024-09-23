30c1e2c1d65b8e6aefe109a7d6f122af98a5ab29
nkmtdak <tanemaki@hotmail.com>
2024-09-23 20:43:48 +0900

    dartsass修正

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -2,5 +2,9 @@ Rails.application.config.dartsass.builds = {
   "application.scss" => "application.css"
 }
 
-Rails.application.config.dartsass.build_options = "--style=compressed --load-path=#{Rails.root.join('app', 'assets', 'stylesheets')}"
+Rails.application.config.dartsass.build_options = [
+  "--style=compressed",
+  "--load-path=#{Rails.root.join('app', 'assets', 'stylesheets')}"
+]
+
 Rails.application.config.dartsass.source_map = false
