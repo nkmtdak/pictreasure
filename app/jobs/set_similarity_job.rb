71ccde352c510d36326903f80df080332cf279f7
nkmtdak <tanemaki@hotmail.com>
2024-09-27 16:58:24 +0900

    Updete_similariry_job.rb

--- app/jobs/set_similarity_job.rb
+++ app/jobs/set_similarity_job.rb
@@ -5,24 +5,25 @@ class SetSimilarityJob < ApplicationJob
     Rails.logger.info "Starting SetSimilarityJob for Photo ID: #{photo_id}"
 
     photo = Photo.find(photo_id)
-    Rails.logger.info "Found photo with ID: #{photo_id}"
-
     similarity = photo.calculate_similarity
-    Rails.logger.info "Calculated similarity for Photo ID: #{photo_id}, Similarity: #{similarity}"
 
     if similarity
-      result = photo.update(similarity:)
-      if result
-        Rails.logger.info "Successfully updated similarity for Photo ID: #{photo_id}"
+      if photo.update(similarity: similarity)
+        Rails.logger.info "Updated similarity for Photo ID: #{photo_id}, Similarity: #{similarity}"
+        
+        # チャレンジのクリア状態を更新
+        if similarity >= 0.7 && !photo.challenge.cleared?
+          photo.challenge.update(cleared_at: Time.current)
+          Rails.logger.info "Challenge cleared for Photo ID: #{photo_id}"
+        end
       else
-        Rails.logger.error "Failed to update similarity for Photo ID: #{photo_id}. Errors: #{photo.errors.full_messages}"
+        Rails.logger.error "Failed to update similarity for Photo ID: #{photo_id}"
       end
     else
       Rails.logger.error "Failed to calculate similarity for Photo ID: #{photo_id}"
     end
   rescue StandardError => e
     Rails.logger.error "Error in SetSimilarityJob for Photo ID: #{photo_id}: #{e.message}"
-    Rails.logger.error e.backtrace.join("\n")
   ensure
     Rails.logger.info "Finished SetSimilarityJob for Photo ID: #{photo_id}"
   end
