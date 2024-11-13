a5afc907279ad753e8ec32cfd4bc2853cf81ef22
nkmtdak <tanemaki@hotmail.com>
2024-11-13 20:50:58 +0900

    ハンバーメニュー

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,4 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "./photo_upload"
-
-document.addEventListener('DOMContentLoaded', function() {
-  const hamburger = document.getElementById('hamburger-menu');
-  const navLinks = document.querySelector('.menu-links');
-
-  hamburger.addEventListener('click', function() {
-    navLinks.classList.toggle('active');
-    hamburger.classList.toggle('active');
-  });
-});
+import "bootstrap"
