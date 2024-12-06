fcda51e1a45518c0e73fabca9bf1d776f8368ce2
nkmtdak <tanemaki@hotmail.com>
2024-12-07 01:11:13 +0900

    SCSS調整

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
-Rails.application.config.assets.precompile += %w( *.scss )
+Rails.application.config.assets.precompile += %w( application.scss )
 Rails.application.config.assets.css_compressor = nil
 Rails.application.config.assets.precompile += %w( *.svg )
